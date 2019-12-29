03bb2d29272bb9209904d22b0bfe5b7c10053fb6
Guangzong Chen <chen-gz@outlook.com>
2019-12-28 20:53:40 -0500

    update Dec 28

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from PySide2 import QtCore, QtWidgets
-from PySide2.QtCore import Qt
+from PySide2.QtCore import Qt, QTime, QDateTime
 from PySide2.QtWidgets import QApplication, QMainWindow, QFileDialog, QMessageBox
-from PySide2.QtGui import QIcon, QPixmap, QPainter
+from PySide2.QtGui import QIcon, QPixmap, QPainter, QColor
 from PySide2.QtCharts import QtCharts
-# import matplotlib.pyplot as plt
+from read_imu import imu
 
 from main_window import Ui_MainWindow
 import logging
@@ -25,22 +25,23 @@ class MainWindow(QMainWindow):
         self.imgLabels = [self.ui.image1, self.ui.image2,
                           self.ui.image3, self.ui.image4, self.ui.image5]
 
-        # self.classifier = classify()
-        # self.classifyResult = []
+        self.classifier = classify()
+        self.imu = imu()
 
-    # def showClassifyChartAll(self):
-    #     self.classifyImageAll()
-    #     self.chart = QtCharts.QChart()
-    #     self.series = QtCharts.QLineSeries()
-    #     # add data
-    #     for i in range(self.imgQuantity):
-    #         self.series.append(i, int(self.classifyResult[i]))
-    #     self.chart.legend().hide()
-    #     self.chart.addSeries(self.series)
-    #     # TODO: X and Y axis setting
-    #     self.chart.createDefaultAxes()
-    #     self.ui.chartView.setChart(self.chart)
-    #     self.ui.chartView.setRenderHint(QPainter.Antialiasing)
+    def showClassifyChart(self):
+        # whole chart
+        self.classifyImage()
+        self.chart = QtCharts.QChart()
+        self.series = QtCharts.QLineSeries()
+        # add data
+        for i in range(self.imgQuantity):
+            self.series.append(i, int(self.classifyResultSet[self.names[i]]))
+        self.chart.legend().hide()
+        self.chart.addSeries(self.series)
+        # TODO: X and Y axis setting
+        self.chart.createDefaultAxes()
+        self.ui.chartView.setChart(self.chart)
+        self.ui.chartView.setRenderHint(QPainter.Antialiasing)
 
     def preload(self):
         self.imgSet = {}
@@ -51,19 +52,15 @@ class MainWindow(QMainWindow):
         self.images = []
         for i in range(self.imgIndex, min(self.imgQuantity, self.imgIndex + 5)):
             self.images.append(self.imgSet[self.names[i]])
+            if self.names[i] not in self.classifyResultSet:
+                result = self.classifier.getClass(self.filePath, self.names[i])
+                self.classifyResultSet[self.names[i]] = result
 
-    # def classifyImage(self):
-    #     self.classifyResult = []
-    #     for item in self.names[self.imgIndex]:
-    #         self.classifyResult.append(
-    #             self.classifier.getClass(self.filePath, item))
-
-    # def classifyImageAll(self):
-    #     self.classifyResult = []
-    #     for item1 in self.names:
-    #         for item in item1:
-    #             self.classifyResult.append(
-    #                 self.classifier.getClass(self.filePath, item))
+    def classifyImage(self):
+        for item in self.names:
+            if item not in self.classifyResultSet:
+                result = self.classifier.getClass(self.filePath, item)
+                self.classifyResultSet[item] = result
 
     def showImages(self):
         self.readImages()
@@ -77,6 +74,19 @@ class MainWindow(QMainWindow):
             for i in range(len(self.images), self.uiImageSize):
                 self.imgLabels[i].clear()
             logging.info("clear unused image label success")
+
+        # local chart
+        chartLocal = QtCharts.QChart()
+        seriesLocal = QtCharts.QLineSeries()
+        # add data
+        for i in range(self.imgIndex, min(self.imgQuantity, self.imgIndex + 5)):
+            seriesLocal.append(i, self.classifyResultSet[self.names[i]])
+        chartLocal.legend().hide()
+        chartLocal.addSeries(seriesLocal)
+        # TODO: X and Y axis setting
+        chartLocal.createDefaultAxes()
+        self.ui.chartView2.setChart(chartLocal)
+        self.ui.chartView2.setRenderHint(QPainter.Antialiasing)
 
     def getImagesName(self):
         self.names = os.listdir(self.filePath)
@@ -135,7 +145,9 @@ class MainWindow(QMainWindow):
         fileName = QFileDialog.getExistingDirectory(self, 'Open Directory')
         logging.info('open fold Directory' + fileName)
         self.filePath = fileName
+        self.ui.statusbar.showMessage("path" + self.filePath)
         self.getImagesName()
+
         if self.imgQuantity == 0:
             self.msg.setText("No image in selected folder")
             self.msg.show()
@@ -143,9 +155,59 @@ class MainWindow(QMainWindow):
             return
         else:
             self.ui.showImageButton.setEnabled(True)
+            self.ui.analysisButton.setEnabled(True)
+            # TODO: clear all variable
+            self.classifyResult = []
+            self.classifyResultSet = {}
 
         # TODO: preload function should be move (other place). Maybe use new thread.
         self.preload()
+
+    def analysis(self):
+        # TODO: judgement folder size
+        self.showClassifyChart()
+
+    def getImuPath(self):
+        fileName = QFileDialog.getOpenFileName(self, 'imu data', '', '*.hex')
+        self.imu.setpath(fileName[0])
+
+    def ImuDataAnalysis(self):
+        self.imu.processImuData()
+        chartAcc, seriesAcc = [], []
+        chartView = [self.ui.chartViewAcc1,self.ui.chartViewAcc2,self.ui.chartViewAcc3]
+        for i in range(3):
+            chartAcc.append(QtCharts.QChart())
+            seriesAcc.append(QtCharts.QLineSeries())
+        # seriesAcc[0].setColor(QColor(255, 0, 0))
+        # seriesAcc[1].setColor(QColor(0, 255, 0))
+        # seriesAcc[2].setColor(QColor(0, 0, 255))
+        # add data
+        timestamp = QTime()
+        dateTime = QDateTime()
+        for i in range(len(self.imu.acc)):
+            timestamp.setHMS(self.imu.timestamp[i][0], self.imu.timestamp[i][1], self.imu.timestamp[i][2])
+            dateTime.setTime(timestamp)
+            for j in range(3):
+                seriesAcc[j].append(dateTime.toMSecsSinceEpoch(), self.imu.acc[i][j])
+        for i in range(3):
+            chartAcc[i].legend().hide()
+            chartAcc[i].addSeries(seriesAcc[i])
+            # X axis setting
+            axisX = QtCharts.QDateTimeAxis()
+            axisX.setTickCount(10)
+            axisX.setFormat("HH:mm:ss")
+            axisX.setTitleText("Time")
+            chartAcc[i].addAxis(axisX, Qt.AlignBottom)
+            seriesAcc[i].attachAxis(axisX)
+            # Y axis setting
+            axisY = QtCharts.QValueAxis()
+            axisY.setTickCount(1)
+            axisY.setLabelFormat('%i')
+            axisY.setTitleText('acc ' + str(i+1))
+            chartAcc[i].addAxis(axisY,Qt.AlignLeft)
+            seriesAcc[i].attachAxis(axisY)
+            chartView[i].setChart(chartAcc[i])
+            chartView[i].setRenderHint(QPainter.Antialiasing)
 
 
 if __name__ == '__main__':
